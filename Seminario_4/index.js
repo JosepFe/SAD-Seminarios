39c29f0a7a46d8a242124c48feb9e44356af949d
Josep Ferrandis <josepfejo1@gmail.com>
2018-11-18 22:21:36 +0100

    Broadcast cuando se conecta un usuario Suporte para nicknames typing funcionalidad How is online(incomplet)

--- Seminario_4/index.js
+++ Seminario_4/index.js
@@ -7,19 +7,58 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  
+  var personName;
+  
+  var peopleTyping = [];
+  var peopleOnline = [];
 
   console.log('a user connected');
-  socket.broadcast.emit('chat message','Se ha conectado un nuevo usuario.');
 
-  
   socket.on('disconnect', function(){
     console.log('user disconnected');
-    socket.broadcast.emit('chat message','Se ha desconectado un usuario.');
+    if(personName == ""){
+      socket.broadcast.emit('chat message','Se ha desconectado un usuario');
+    } else {
+      var index = peopleOnline.indexOf(personName);
+      if (index > -1) {
+        peopleOnline.splice(index, 1);
+     }
+      socket.broadcast.emit('chat message','Se ha desconectado ' + personName);
+    }
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    if(personName == "" || personName == null){
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    } else {
+      var index = peopleTyping.indexOf(personName);
+      if (index > -1) {
+        peopleTyping.splice(index, 1);
+        io.emit('stop typing', personName);
+     }
+      console.log('message: ' + personName + ": " + msg);
+      io.emit('chat message', personName + ": " + msg);
+    }
+  });
+
+  socket.on('escribiendo', function(person){
+    if(person != "" && personName != null && !peopleTyping.includes(person)){
+      socket.broadcast.emit('typing', person + ' is typing')
+      peopleTyping.push(person);
+    }
+  });
+
+  socket.on('nombre persona', function(person){
+    peopleOnline.push(person);
+    console.log(person);
+    personName = person;
+    if(person == "" || personName == null){
+      socket.broadcast.emit('chat message','Se ha conectado un nuevo usuario');
+    }else{
+      socket.broadcast.emit('chat message','Se ha conectado ' + person);
+    }
   });
 
 });
