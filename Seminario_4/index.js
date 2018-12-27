9b245d88bcae9e4cea2085bf414d6c6324350d93
Josep Ferrandis <josepfejo1@gmail.com>
2018-12-27 23:20:51 +0100

    Add private message funcionality

--- Seminario_4/index.js
+++ Seminario_4/index.js
@@ -8,10 +8,11 @@ app.get("/", function(req, res) {
 
 var peopleOnline = [];
 var peopleTyping = [];
+var users = [];
 
 io.on("connection", function(socket) {
   var personName;
-  console.log("a user connected");
+  console.log("a user connected with id" + socket.id);
 
   socket.on("disconnect", function() {
     console.log("user disconnected");
@@ -20,14 +21,33 @@ io.on("connection", function(socket) {
     if (index > -1) {
       peopleOnline.splice(index, 1);
     }
-    io.emit('user list', peopleOnline);
-    socket.emit('stop typing', personName);
+    io.emit("user list", peopleOnline);
+    socket.emit("stop typing", personName);
     socket.broadcast.emit("chat message", "Se ha desconectado " + personName);
   });
 
   socket.on("chat message", function(msg) {
-    console.log("message: " + personName + ": " + msg);
-    io.emit("chat message", personName + ": " + msg);
+    if (msg.charAt(0) == "%") {
+
+      var aux = msg.split("%");
+      var nameTo = aux[1];
+      var message = aux[2];
+      var idTo = "";
+
+      users.forEach(user => {
+        if (user[0] == nameTo) {
+          idTo = user[1];
+        }
+      });
+      console.log(
+        "el mensaje de " + personName + " para " + nameTo + " es : " + message
+      );
+
+      socket.to(idTo).emit("chat message", personName + ":" + message);
+    } else {
+      console.log("el mensaje publico de " + personName + " es : " + message);
+      io.emit("chat message", personName + ": " + msg);
+    }
   });
 
   socket.on("escribiendo", function(person) {
@@ -37,7 +57,7 @@ io.on("connection", function(socket) {
     }
   });
 
-  socket.on('ha dejado de escribir', function(person){
+  socket.on("ha dejado de escribir", function(person) {
     var index = peopleTyping.indexOf(person);
     if (index > -1) {
       peopleTyping.splice(index, 1);
@@ -46,11 +66,13 @@ io.on("connection", function(socket) {
   });
 
   socket.on("nombre persona", function(person) {
+    var persona = [person, socket.id];
+    users.push(persona);
     peopleOnline.push(person);
-    console.log(person);
+    console.log(users);
     personName = person;
     socket.broadcast.emit("chat message", "Se ha conectado " + person);
-    io.emit('user list', peopleOnline);
+    io.emit("user list", peopleOnline);
   });
 });
 
