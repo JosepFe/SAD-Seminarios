c2cdc16b51560b8c989ca724535f2f06ae1e757b
Josep Ferrandis <josepfejo1@gmail.com>
2018-12-27 11:26:23 +0100

    Support for nicknaes Who is online Fix typing ------------------------ Missing:  - Private messages  - Fix typing and who is online style

--- Seminario_4/index.js
+++ Seminario_4/index.js
@@ -1,68 +1,59 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  
+var peopleOnline = [];
+var peopleTyping = [];
+
+io.on("connection", function(socket) {
   var personName;
-  
-  var peopleTyping = [];
-  var peopleOnline = [];
+  console.log("a user connected");
 
-  console.log('a user connected');
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    if(personName == ""){
-      socket.broadcast.emit('chat message','Se ha desconectado un usuario');
-    } else {
-      var index = peopleOnline.indexOf(personName);
-      if (index > -1) {
-        peopleOnline.splice(index, 1);
-     }
-      socket.broadcast.emit('chat message','Se ha desconectado ' + personName);
+    var index = peopleOnline.indexOf(personName);
+    if (index > -1) {
+      peopleOnline.splice(index, 1);
     }
+    io.emit('user list', peopleOnline);
+    socket.emit('stop typing', personName);
+    socket.broadcast.emit("chat message", "Se ha desconectado " + personName);
   });
 
-  socket.on('chat message', function(msg){
-    if(personName == "" || personName == null){
-      console.log('message: ' + msg);
-      io.emit('chat message', msg);
-    } else {
-      var index = peopleTyping.indexOf(personName);
-      if (index > -1) {
-        peopleTyping.splice(index, 1);
-        io.emit('stop typing', personName);
-     }
-      console.log('message: ' + personName + ": " + msg);
-      io.emit('chat message', personName + ": " + msg);
-    }
+  socket.on("chat message", function(msg) {
+    console.log("message: " + personName + ": " + msg);
+    io.emit("chat message", personName + ": " + msg);
   });
 
-  socket.on('escribiendo', function(person){
-    if(person != "" && personName != null && !peopleTyping.includes(person)){
-      socket.broadcast.emit('typing', person + ' is typing')
+  socket.on("escribiendo", function(person) {
+    if (!peopleTyping.includes(person)) {
+      socket.broadcast.emit("typing", person + " is typing");
       peopleTyping.push(person);
     }
   });
 
-  socket.on('nombre persona', function(person){
-    peopleOnline.push(person);
-    console.log(person);
-    personName = person;
-    if(person == "" || personName == null){
-      socket.broadcast.emit('chat message','Se ha conectado un nuevo usuario');
-    }else{
-      socket.broadcast.emit('chat message','Se ha conectado ' + person);
+  socket.on('ha dejado de escribir', function(person){
+    var index = peopleTyping.indexOf(person);
+    if (index > -1) {
+      peopleTyping.splice(index, 1);
+      io.emit("stop typing", person);
     }
   });
 
+  socket.on("nombre persona", function(person) {
+    peopleOnline.push(person);
+    console.log(person);
+    personName = person;
+    socket.broadcast.emit("chat message", "Se ha conectado " + person);
+    io.emit('user list', peopleOnline);
+  });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
