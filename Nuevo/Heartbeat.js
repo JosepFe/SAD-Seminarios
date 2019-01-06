4b973f1b52148c0f6461067fee2305e1ed7af71a
Josep Ferrandis <josepfejo1@gmail.com>
2019-01-06 22:29:17 +0100

    primera aproximación al Heartbeat

--- Nuevo/Heartbeat.js
+++ Nuevo/Heartbeat.js
@@ -7,23 +7,23 @@ socket.bindSync("tcp://127.0.0.1:3000");
 const topic = "heartbeat";
 
 var listWorkers = [];
-var newListWorkers = [];
+var auxListWorkers = [];
 
 setInterval(function() {
   const timestamp = Date.now().toString();
   socket.send([topic, timestamp]); // Publish timestamp
-}, 4000);
+}, 300);
 
 rep.on("message", function(data) {
   //console.log(data.toString());
   rep.send("message", "ACK");
-  if (!listWorkers.includes(data.toString())) {
-    newListWorkers.push(data.toString());
-    //console.log(listWorkers);
+  if (!auxListWorkers.includes(data.toString())) {
+    auxListWorkers.push(data.toString());
   }
 });
 
 setInterval(function() {
-    listWorkers = newListWorkers;
-    console.log(listWorkers);
-  }, 3000);
+  listWorkers = newListWorkers;
+  newListWorkers = [];
+  console.log(listWorkers);
+}, 400);
